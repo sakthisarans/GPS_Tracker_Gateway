6c314829fca8a467de35f5f179dd7579fadd0a7e
sakthisaranprjx <sakthisaran.ksr@gmail.com>
2024-06-21 02:54:12 +0530

    1st commit

--- src/main/java/com/sakthi/gateway/SampleController.java
+++ src/main/java/com/sakthi/gateway/SampleController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleController {
     @GetMapping("/ping")
     public ResponseEntity<String> ping(){
+
         return new ResponseEntity<>("OK", HttpStatus.OK);
     }
 }
