fcdf3a8e19c429fe6e8daa0fae6d86af42aa0d6b
sakthisaranprjx <sakthisaransss55@gmail.com>
2024-07-18 10:46:19 +0530

    cors config

---
+++ src/main/java/com/sakthi/gateway/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.sakthi.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig extends CorsConfiguration {
+    @Bean
+    public CorsWebFilter corsFilter()
+    {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials( true );
+        config.setAllowedOrigins( List.of( "http://localhost:800" ) );
+        config.setAllowedMethods( List.of( "GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD" ) );
+        config.setAllowedHeaders( List.of( "*" ) );
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration( "/**", config );
+
+        return new CorsWebFilter( source );
+    }
+}
